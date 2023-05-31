34321d411c5cad683f83c999fa011819c857c47f
qkdwngus <arkhyeon@gmail.com>
2023-05-31 17:57:04 +0900

    [feat] quiz setting

---
+++ src/pages/summary/OrderSummary.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function OrderSummary(props) {
+  return <div></div>;
+}
+
+export default OrderSummary;
---
+++ src/pages/summary/SummaryForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SummaryForm(props) {
+  return <div></div>;
+}
+
+export default SummaryForm;
